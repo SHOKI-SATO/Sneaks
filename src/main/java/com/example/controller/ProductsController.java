29549ba2e2fadc0b42618911ae3b2d22d70c2430
windcalm666 <79242740+windcalm666@users.noreply.github.com>
2022-01-27 04:17:22 +0900

    topページ表示

--- src/main/java/com/example/controller/ProductsController.java
+++ src/main/java/com/example/controller/ProductsController.java
@@ -1,11 +1,15 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.user.model.MUser;
+import com.example.domain.user.service.UserService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,13 +18,25 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ProductsController {
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping("/top")
 	public String getTop(ModelMap model) {
 		//ログインしているユーザー情報の獲得
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		String name = auth.getName();
 		
-		model.addAttribute("username", name);
+		//取得したユーザー情報から、ユーザーIDを取得する
+		String id = auth.getName();
+		
+		//ユーザーIDからユーザーを獲得
+		MUser user = userService.getUserOne(id);
+		String name = user.getUserName();
+		
+		log.info(name);
+		
+		model.addAttribute("userId", id);
+		model.addAttribute("userName", name);
 		return "/top/S_TOP";
 	}
 
